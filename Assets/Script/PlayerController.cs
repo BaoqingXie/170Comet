dab47b434d264e358ce7dc39f006c203bc69fa9c
BaoqingXie <39812037+BaoqingXie@users.noreply.github.com>
2020-11-24 13:06:22 +0800

    Revert "throw snowball"
    This reverts commit cd6fe1f16f8085b304deceb74e1fb0ec69c8947f.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // 可以在controller看到的变量
-    [SerializeField]
-    private Rigidbody2D rb;
-    //public float speed;
-    public float move_speed;
-    private bool facing_right = true;
-    private float move_x;
-
+    [SerializeField]private Rigidbody2D rb;
+    public float speed;
     public float jumpforce;
 
 
@@ -21,74 +16,29 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update(){
-
-        player_move();
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        // char jump
         if(Input.GetButtonDown("Jump")/*&& coll.IsTouchingLayers(ground)*/){
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
-             // anim.SetBool("jumping", true); 
+            // anim.SetBool("jumping", true); 
+        }
+    }
+
+    void Movement(){
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedirection = Input.GetAxisRaw("Horizontal");
+
+        // char move
+        if(horizontalmove != 0){
+            rb.velocity = new Vector2(horizontalmove*speed*Time.deltaTime, rb.velocity.y);
+        }
+
+        if(facedirection != 0){
+            transform.localScale = new Vector3(2*facedirection, 2, 1);
         }
 
     }
-
-    //functions used for all the player actions
-    void player_move(){
-
-        //control
-        move_x = Input.GetAxisRaw("Horizontal");
-
-        //animation
-
-        //player direction
-        if(move_x > 0.0f && facing_right == false){
-
-            flip_player();
-
-        }
-        else if(move_x < 0.0f && facing_right == true){
-
-            flip_player();
-
-        }
-        //physics
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (move_x * move_speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
-
-
-    }
-
-    //function used to flip the character sprite alone the x axis
-    void flip_player(){
-
-        facing_right = !facing_right;
-        Vector2 local_scale = gameObject.transform.localScale;
-        local_scale.x *= -1;
-        transform.localScale = local_scale;
-
-    }
-
-    // Update is called once per frame
-    // void FixedUpdate()
-    // {
-    //     Movement();
-    //     // char jump
-    //     if(Input.GetButtonDown("Jump")/*&& coll.IsTouchingLayers(ground)*/){
-    //         rb.velocity = new Vector2(rb.velocity.x, jumpforce);
-    //         // anim.SetBool("jumping", true); 
-    //     }
-    // }
-
-    // void Movement(){
-    //     float horizontalmove = Input.GetAxis("Horizontal");
-    //     float facedirection = Input.GetAxisRaw("Horizontal");
-
-    //     // char move
-    //     if(horizontalmove != 0){
-    //         rb.velocity = new Vector2(horizontalmove*speed*Time.deltaTime, rb.velocity.y);
-    //     }
-
-    //     if(facedirection != 0){
-    //         transform.localScale = new Vector3(2*facedirection, 2, 1);
-    //     }
-
-    // }
 }
